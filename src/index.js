049ff5c7658ac493c9e26f6a93255407e352e636
Alexandr Drozd <alex.drozd.fd@gmail.com>
2021-08-30 01:28:06 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,71 @@
 function eval() {
-    // Do not use eval!!!
-    return;
+  // Do not use eval!!!
+  return;
 }
 
-function expressionCalculator(expr) {
-    // write your solution here
+const expressionCalculator = (str) => {
+  const operations = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => {
+      if (b === 0) {
+        throw 'TypeError: Division by zero.';
+      }
+      return (a / b);
+    }
+  }
+  const operators = Object.keys(operations);
+
+  const processExpression = (expr) => {
+    for (let operator of operators) {
+      let regexPattern = `(?!^)(?<!\\${operators.map(el => '\\' + el).join('|')})\\${operator}`;
+      let found = [...expr.matchAll(new RegExp(regexPattern, 'g'))];
+      let matchedOperation = found[found.length - 1];
+
+      if (matchedOperation) {
+        let x = expr.slice(0, matchedOperation.index);
+        let y = expr.slice(matchedOperation.index + 1);
+
+        return y ? operations[matchedOperation[0]](processExpression(x), processExpression(y)) : x;
+      }
+    }
+
+    return +expr;
+  }
+
+  const calculate = (str) => {
+    str = str.replace(/\s/g, '');
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === ')') {
+        if (!stack.length) {
+          throw 'ExpressionError: Brackets must be paired';
+        }
+
+        let toCalculate = str.slice(stack[stack.length - 1] + 1, i);
+
+        let calculated = processExpression(toCalculate);
+
+        str = str.slice(0, stack[stack.length - 1]) + calculated + str.slice(i + 1);
+
+        i = stack.pop();
+
+      } else if (str[i] === '(') {
+        stack.push(i);
+      }
+    }
+    if (stack.length) {
+      throw 'ExpressionError: Brackets must be paired';
+    }
+    return processExpression(str);
+  }
+
+  return calculate(str);
+
 }
 
 module.exports = {
-    expressionCalculator
+  expressionCalculator
 }
